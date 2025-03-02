34780a92837709bf1b78b4db13203e7c4246033e
Furosha <levent.kyllian@gmail.com>
2025-03-02 22:14:51 +0100

    Refactor audio device synchronization to use notifications instead of a timer

--- Service1.cs
+++ Service1.cs
@@ -10,43 +10,41 @@ namespace Audi_Auto_Switch
 {
     public partial class Service1 : ServiceBase
     {
-        private Timer timer;
         private MMDeviceEnumerator deviceEnumerator;
+        private MMDeviceNotificationClient notificationClient;
 
         public Service1()
         {
             InitializeComponent();
             deviceEnumerator = new MMDeviceEnumerator();
+            notificationClient = new MMDeviceNotificationClient(SynchronizeAudioDevices);
         }
 
         protected override void OnStart(string[] args)
         {
-            // Vérifier toutes les 2 secondes
-            timer = new Timer(SynchronizeAudioDevices, null, 0, 2000);
+            deviceEnumerator.RegisterEndpointNotificationCallback(notificationClient);
+            // Synchroniser une première fois au démarrage
+            SynchronizeAudioDevices();
         }
 
         protected override void OnStop()
         {
-            timer?.Dispose();
+            deviceEnumerator.UnregisterEndpointNotificationCallback(notificationClient);
             deviceEnumerator?.Dispose();
         }
 
-        private void SynchronizeAudioDevices(object state)
+        private void SynchronizeAudioDevices()
         {
+            // Même code que précédemment, mais sans le paramètre object state
             try
             {
-                // Obtenir le périphérique de lecture par défaut
                 var defaultPlaybackDevice = deviceEnumerator.GetDefaultAudioEndpoint(
                     DataFlow.Render, Role.Multimedia);
-
-                // Obtenir le périphérique de communication actuel
                 var currentCommunicationDevice = deviceEnumerator.GetDefaultAudioEndpoint(
                     DataFlow.Render, Role.Communications);
 
-                // Si les périphériques sont différents, synchroniser
                 if (defaultPlaybackDevice.ID != currentCommunicationDevice.ID)
                 {
-                    // Définir le périphérique de communication par défaut
                     using (var policyConfig = new PolicyConfigClient())
                     {
                         policyConfig.SetDefaultEndpoint(defaultPlaybackDevice.ID, Role.Communications);
@@ -70,10 +68,11 @@ namespace Audi_Auto_Switch
         int SetDefaultEndpoint(string deviceId, Role role);
     }
 
-    internal class PolicyConfigClient
+    internal class PolicyConfigClient : IDisposable
     {
         private static readonly Guid CLSID_PolicyConfig = Guid.Parse("870af99c-171d-4f9e-af0d-e63df40c2bc9");
         private readonly IPolicyConfig _policyConfig;
+        private bool _disposed = false;
 
         public PolicyConfigClient()
         {
@@ -82,7 +81,45 @@ namespace Audi_Auto_Switch
 
         public void SetDefaultEndpoint(string deviceId, Role role)
         {
+            if (_disposed)
+                throw new ObjectDisposedException(nameof(PolicyConfigClient));
+                
             _policyConfig.SetDefaultEndpoint(deviceId, role);
         }
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                if (_policyConfig != null && Marshal.IsComObject(_policyConfig))
+                {
+                    Marshal.ReleaseComObject(_policyConfig);
+                }
+                _disposed = true;
+            }
+        }
+    }
+
+    internal class MMDeviceNotificationClient : NAudio.CoreAudioApi.Interfaces.IMMNotificationClient
+    {
+        private readonly Action _onDeviceChanged;
+
+        public MMDeviceNotificationClient(Action onDeviceChanged)
+        {
+            _onDeviceChanged = onDeviceChanged;
+        }
+
+        public void OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId)
+        {
+            if (flow == DataFlow.Render && role == Role.Multimedia)
+            {
+                _onDeviceChanged();
+            }
+        }
+
+        public void OnDeviceAdded(string pwstrDeviceId) { }
+        public void OnDeviceRemoved(string deviceId) { }
+        public void OnDeviceStateChanged(string deviceId, DeviceState newState) { }
+        public void OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key) { }
     }
 }
